63b021cf4eaa03d1a8f02d27e3527642dcf403f7
ふうき255 <luy18667@gmail.com>
2025-08-15 21:10:08 +0800

    Math.hpp の作成

---
+++ Math.hpp
@@ -0,0 +1,23 @@
+#ifndef _DF_MATH_HPP_
+#define _DF_MATH_HPP_
+
+
+#ifndef _DF_DATAFRAME_HPP_
+#include "Types/DataFrame.hpp"
+#endif
+
+
+
+DfProcess& DfSqrt(DfProcess process) {
+
+}
+
+
+DfProcess& Df2(DfProcess& 
+
+
+
+
+
+
+#endif // _DF_MATH_HPP_
